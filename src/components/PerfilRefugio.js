d296336e9c78cd35a0d749e7178a5a7fa80d6b60
PaolaRoMa <92270132+PaolaRoMa@users.noreply.github.com>
2024-10-02 10:17:43 -0600

    Aparece la hora en los eventos

--- src/components/PerfilRefugio.js
+++ src/components/PerfilRefugio.js
@@ -270,7 +270,7 @@ const TarjetaPerros = ({imagen, nombre, edad, tamanio, descripcion}) => {
 
 
 
-const TarjetaEventos = ({imagen, nombre, descripcion, fecha, ubicacion, hora, footer}) => {
+const TarjetaEventos = ({imagen, nombre, descripcion, fecha, ubicacion, hora_evento, footer}) => {
   return(
       <div className="row">
           <div className="card mb-3" style={{marginTop: '10px'}}>
@@ -283,7 +283,7 @@ const TarjetaEventos = ({imagen, nombre, descripcion, fecha, ubicacion, hora, fo
                         <h5 className="card-title">{nombre} </h5>
                         <p className="card-text">{descripcion}<br/>
                             <b>Ubicación: </b>{ubicacion}<br/>
-                            <b>Hora: </b>{hora}<br/>
+                            <b>Hora: </b>{hora_evento}<br/>
                             <b>Fecha del evento: </b>{fecha}   
                         </p>
                     </div>
@@ -339,9 +339,7 @@ const HeadSection = ({ profile_image, image1, image2, image3, titulo, descripcio
                               <a href={facebook}><i className="fa-brands fa-facebook" style={{ marginTop: '20px', fontSize: '1.5rem', color: '#070B83' }}></i></a>
                               <a href={instagram}><i className="fa-brands fa-instagram" style={{ marginTop: '20px', marginLeft: '20px', color: '#B817A9', fontSize: '1.5rem' }}></i></a>
                               <div>
-                                <Button variant="primary" onClick={handleShowRegistro}>
-                                  Registrar un perro
-                                </Button>
+
                               </div>  
                           </div>
                       </div>
@@ -390,6 +388,7 @@ const HeadSection = ({ profile_image, image1, image2, image3, titulo, descripcio
                               descripcion={evento.descripcion_evento}
                               fecha={evento.fecha_evento}
                               ubicacion={evento.lugar_evento}
+                              hora_evento={evento.hora_evento}
                               
                               footer={
                                 <div className="d-flex justify-content-end" style={{ backgroundColor: 'transparent' }}>
